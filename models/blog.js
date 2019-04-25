89af2fa4f6601f731e798d98f367335b009af162
lesore0789 <lesore0789@gmail.com>
2019-04-25 16:53:27 -0500

    Add author to blog schema model

--- models/blog.js
+++ models/blog.js
@@ -7,6 +7,13 @@ var blogSchema = new mongoose.Schema({
     image: String,
     body: String,
     created: {type: Date, default: Date.now},
+    author: {
+      id: {
+        type: mongoose. Schema.Types.ObjectId,
+        ref: "User"
+      },
+      username: String
+    },
     comments: [
         {
            type: mongoose.Schema.Types.ObjectId,
